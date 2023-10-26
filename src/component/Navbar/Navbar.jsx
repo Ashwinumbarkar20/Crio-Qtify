d4ceb47b717ada2acbb50259d0cdf38e3787720d
Ashwin Umbarkar <ashwinumbarkar20@gmail.com>
2023-10-26 10:01:59 +0530

    c3:Button

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './Navbar.module.css'
-
+import '../../index.css'
+import Button from '../Button/Button'
 export default function Navbar() {
   return (
     <div>
-      <div className={`${styles.nav} button`}>
+      <div className={`${styles.nav}`}>
         <div>Logo</div>
         <div>Search bar</div>
-        <div>Sharefeedback</div>
+        <Button buttonName="Give Feedback"></Button>
       </div>
     </div>
   )
