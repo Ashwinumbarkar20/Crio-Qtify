e04aceec5d753cfc8b82c67aa30f26174c35fe0b
Ashwin Umbarkar <ashwinumbarkar20@gmail.com>
2023-10-30 11:48:00 +0530

    Creating /adding Album -qtify-takehome

--- src/component/Container/Container.jsx
+++ src/component/Container/Container.jsx
@@ -3,17 +3,23 @@ import styles from "./Container.module.css"
 import Card from '../Card/Card'
 export default function Container({albumData}) {
   return (
-    <div>
+    <div >
+
       <div className={`${styles.containerTitle}`}>
+        
         <span>Top Album</span>
         <span>Show All</span>
       </div>
+
       <div className={`${styles.cards}`}>
-
+<div className='  row'>
 {
-    albumData.map((a)=> <Card key={Math.random()*12} albumimg={a.image} Follows={a.follows} title={a.title}/>)
-}
-
+  albumData.map((a) => (
+            <div className={`${styles.carddiv} col-lg-2 col-md-2.5 col-sm-6`} key={Math.random() * 12}>
+              <Card albumimg={a.image} Follows={a.follows} title={a.title} />
+            </div>
+          ))}
+</div>
       </div>
     </div>
   )
